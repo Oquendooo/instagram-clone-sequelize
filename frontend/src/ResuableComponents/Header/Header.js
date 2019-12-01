c09a46399e22895ae620fb7158819fda8210dfc9
Aaron <iaaron94@gmail.com>
2019-12-01 14:44:07 -0500

    more frontend changes

--- frontend/src/ResuableComponents/Header/Header.js
+++ frontend/src/ResuableComponents/Header/Header.js
@@ -2,7 +2,26 @@ import React, { useState } from 'react';
 import './Header.scss';
 
 const Header = () => {
+
+
+
+  const handleScroll = () => { 
+      const scroll = window.scrollY;
+      const header = document.getElementById('header');
+      const rightSection = document.querySelector('.feedPage__rightSection');
+   
+
+   
+      if (scroll >= 60) {
+        header.classList.add('header-fixed');
+        // rightSection.classList.add('feedPage__rightSection--fixed');
+      } else {
+        header.classList.remove('header-fixed');
+        // rightSection.classList.remove('feedPage__rightSection--fixed');
+      }
+  }
   
+  window.addEventListener('scroll', handleScroll);
   return(
     <div id="header">
       <div className="header-inner container">
--- frontend/src/ResuableComponents/Post/Post.js
+++ frontend/src/ResuableComponents/Post/Post.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import './Post.scss';
 
-const Post = () => {
-  
+const Post = ({post}) => {
+  console.log("post", post);
   const image = 'https://images.pexels.com/photos/3133688/pexels-photo-3133688.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260';
-
+  const testArray = [1,2];
   return(
     <div id="post">
       <div className="post__header">
-        <img src="https://pbs.twimg.com/profile_images/953000038967468033/n4Ngwvi7.jpg" />
+        <img src={post.userIcon} />
         <div className="post__username__and__options">
           <div className="post__username">
-            aaron.shines
+            {post.username}
           </div>
           <div className="post__options">
-            <i class="fas fa-ellipsis-h"></i>
+            <i className="fas fa-ellipsis-h"></i>
           </div>
         </div>
       </div>
-      <div className="post__image" style={{background: `url('${image}'`}} >
-        {/* <img src="" alt=""/> */}
+      <div className="post__image" style={{background: `url('${post.image}'`}} >
       </div>
       <div className="post__actions">
         <div className="post__left_actions">
@@ -28,12 +27,43 @@ const Post = () => {
           <i class="fas fa-share-alt"></i>
         </div>
         <div className="post__right_actions">
-          <i class="far fa-bookmark"></i>
+          <i className="far fa-bookmark"></i>
         </div>
       
       </div>
       <div className="post__engagement">
-        
+
+        <div className="post__likes">
+          146 likes
+        </div>
+
+        <div className="post__post_content">
+          <div className="post__poster_username">
+            aaron.shines
+          </div>
+          <div className="post__body">
+            Always True To Self
+          </div>
+        </div>
+        <div className="post__viewall_button_wrapper">
+          <span className="post__viewall_button">
+          View all 18 comments
+          </span>
+        </div>
+        <div className="post__comments">
+          {testArray.map( comment => {
+            return(
+              <div className="post__comment">
+                <div className="post__commenter_username">
+                james.malroy
+                </div>
+                <div className="post__comment_body">
+                  Always True To Self
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="post__comment_input">
         
